b2a09c7b0d5f33b846b3dd0f5ed3cc2ee4243d65
Varadthorat <57125047+Varadthorat@users.noreply.github.com>
2019-11-21 02:12:29 -0600

    changed some of  the text and added a button

--- src/App.js
+++ src/App.js
@@ -12,13 +12,16 @@ function App() {
         
       </Navbar>
       <Row>
-        <h1>yo</h1>
-        <p>Hi!</p>
-        <p>bye!</p>
+        <h1>Welcome to GDC Squared</h1>
+        <p>Here we can put more information about the event</p>
       </Row>
       <ButtonToolbar>
-        <Button variant = "primary"> Blue!</Button>
-        {/* put the button stuff here */}
+        <Button variant = "primary"> Click here if you want to register.</Button>
+        {/*we can link the application here */}
+      </ButtonToolbar>
+      <ButtonToolbar>
+        <Button variant = "primary"> Click here if you want to sponsor this event.</Button>
+        {/*we can put the link for sponsors here */}
       </ButtonToolbar>
       
     </Container>

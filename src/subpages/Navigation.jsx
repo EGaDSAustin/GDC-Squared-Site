5f24b97f3b25a7fdf2595d46ca3246e83b41a2c8
Katherine Byers <55606873+pingwoh@users.noreply.github.com>
2019-11-28 21:45:21 -0600

    thanksgiving navbar / sponsor update

---
+++ src/subpages/Navigation.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Navbar, Container, Row, Col, Nav, Image} from 'react-bootstrap';
+
+function Navigation() {
+    return (
+        <Navbar expand = "lg" bg="dark" variant="dark" sticky = "top"> 
+            {/* https://react-bootstrap.github.io/components/navbar/ */}
+            <Navbar.Brand href="#home">
+                <img
+                    src="/logo.svg" 
+                    width="390" //will put text in middle; dunno if this will image later image
+                    className="d-inline-block align-top"
+                    alt="GDC^2 logo" 
+                /> 
+            </Navbar.Brand>
+            <Nav className = "mr-auto">
+                <Nav.Link href="#about">ABOUT</Nav.Link>
+                <Nav.Link href="#schedule">SCHEDULE</Nav.Link>
+                <Nav.Link href="#sign-up">SIGN UP</Nav.Link>
+                <Nav.Link href="#join">JOIN</Nav.Link>
+            </Nav>
+        </Navbar>
+    );
+}
+
+export default Navigation;

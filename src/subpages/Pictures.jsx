fe05ad9cf32ec282d00f03491c79f092b252e224
ilm353 <ian_mcdonald@rocketmail.com>
2020-01-28 16:36:31 -0600

    splash page

--- src/subpages/Pictures.jsx
+++ src/subpages/Pictures.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
-import Image from 'react-bootstrap/Image';
 import Header from '../components/Header';
 import Text from '../components/Text';
-
+import Splash from '../splash.png'
 function importAll(r){
   return r.keys().map((item, idx) => {
     return {
@@ -16,33 +15,21 @@ function importAll(r){
 
 const images = importAll(require.context('../photos', false, /\.(png|jpe?g|svg)$/));
 
-const pictureList = [
-  {
-    name: 'Picture Slide 1',
-    img: './photos/51958000_308670329851690_6361093117973626880_n.jpg',
-    altText: 'Picture Slide 1'
-  },
-  {
-      name: 'Picture Slide 2',
-      img: 'holder.js/100px250',
-      altText: 'Picture Slide 2'
-  },
-  {
-      name: 'Picture Slide 3',
-      img: 'holder.js/100px250',
-      altText: 'Picture Slide 3'
-  },
-]
-
-
-
 const Pictures = (props) => {
   return (
     <div>
-      <Header>GDC<sup>2</sup></Header>
-      <div bsPrefix="subpage1-row">
-        <Header primary center>Be there or be SQUARED<sup>2</sup> (he he get it)</Header>
-        <Text primary center style={{color:"black"}}>February 22, 2020</Text>
+      <div
+      style={{
+        background: `url(${Splash})`,
+        backgroundSize: 'cover',
+        width : '100vw',
+        height : '98vh',
+        color  : 'white'
+      }}
+      >
+        <Header primary center style={{color: 'white', paddingBottom:0}}>Be there or be squared</Header>
+        <Header>GDC<sup>2</sup></Header>
+        <Text primary center>February 22, 2020</Text>
       </div>
       
       <Carousel

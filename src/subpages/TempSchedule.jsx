2555e301ffe689acdb5de3fb7bc4efdd48b86ff8
ilm353 <ian_mcdonald@rocketmail.com>
2020-02-06 14:34:22 -0600

    join the discord

--- src/subpages/TempSchedule.jsx
+++ src/subpages/TempSchedule.jsx
@@ -7,7 +7,7 @@ import Header from '../components/Header';
 function TempSchedule (props) {
     return <div id='schedule'>
             <Header primary>Schedule</Header>
-            <Text center>Coming Soon!</Text>
+            <Text center>Coming Soon! Join the <a href={'https://discord.gg/4ShtMPx'}>Discord</a> for anouncements</Text>
         </div>
 }
 

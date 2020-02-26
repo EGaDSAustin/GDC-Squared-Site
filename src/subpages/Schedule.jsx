da11ea7e9357d559d044dd7782b63607d688f65e
Ali Kedwaii <ali.kedwaii@utexas.edu>
2020-02-26 15:07:32 -0600

    Panel adjustments in Schedule

--- src/subpages/Schedule.jsx
+++ src/subpages/Schedule.jsx
@@ -5,13 +5,13 @@ const Schedule = (props) => {
 
     const schedule = [ ['Time'         ,'2.216','4.302','5.302','6.302','5.304'],
                        ['11:00-11:30AM','Kickoff','','','',''],
-                       ['11:30-12:30PM','','Eduardo Ortiz, Kat Wenske, & Daniel Rosenfeld Mod. Tyler Coleman: Game Audio Panel','','Phillip Johnson & Gordon Walton: Game Marketing',''],
+                       ['11:30-12:30PM','','Eduardo Ortiz, Kat Wenske, & Daniel Rosenfeld Mod. Tyler Coleman: Game Audio Panel','','Phillip Johnson, Cricket Carlson, Chip Thurston & Elisa Reyna: Game Marketing Panel',''],
                        ['12:30-1:30PM' ,'','','Alex Keller: Sound Design Workshop','','Ava Pek: Narrative Scripting Languages'],
                        ['1:30-2:30PM'  ,'Lunch/Showcase (Atrium)','','','',''],
                        ['2:30-3:30PM'  ,'Game Pitch Competition','Daniel Reese & Fox Buchele: Blender Workshop','','Bread & Butter Game Co.: "Shipping a Company" Panel',''],
                        ['3:30-4:30PM'  ,'Game Pitch Competition','','Michael Baker: Crafting Game Feel Workshop','','Richard Terrell: Level Design in Mario Maker 2'],
                        ['4:30-5:30PM'  ,'Game Pitch Competition','Nabi Barak: Qualitative Feedback in Gameplay Design','','Etienne Vouga: "Zelda Classic" Gamemaker',''],
-                       ['5:30-6:30PM'  ,'Richard Terrell & Susan O\'Connor: Collaborating Across Disciplines','','','',''],
+                       ['5:30-6:30PM'  ,'Richard Terrell, Gordon Walton, Susan O\' Connor & Chip Sbrogna: Collaborating Across Disciplines','','','',''],
                        ['6:30-7:00PM'  ,'Closing Ceremony','','','',''],
                     ];
     
